ca7fd36830d46f6edeb1a09e0d8dfa2f39e2144e
João Freire <joao.m.c.freire@gmail.com>
2022-11-06 16:30:40 +0100

    DELETE Add example scene and test script

---
+++ Assets/Tests/TestRectangleGestureinteraction/GamepadTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamepadTest : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started!");
+
+        // InputSystem.RegisterInteraction<RectangleGestureInteraction>();
+
+        var keyboard = InputSystem.AddDevice<Mouse>();
+        var mouseRectangleMovement = new InputAction("RectangleMovement", binding: "<Pointer>/position", interactions: "RectangleGesture");
+
+        mouseRectangleMovement.Enable();
+        mouseRectangleMovement.performed += ctx =>
+        {
+            Debug.Log("My mouse has drawn a rectangle!: ");
+        };
+
+        mouseRectangleMovement.canceled += ctx =>
+        {
+            Debug.Log("Canceled!");
+        };
+
+    }
+}
